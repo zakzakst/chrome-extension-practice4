99fdb41c66634d9d5ed85c719df5839958251328
zakzakst <e1433104@gmail.com>
2026-05-22 14:41:54 +0900

    fix

--- src/pages/options/App.tsx
+++ src/pages/options/App.tsx
@@ -1,20 +1,86 @@
+import { saveTextSize, loadTextSize } from "@/src/shared/storage/textSize";
+import { useCallback, useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { toast } from "sonner";
+
+type TextSizeItem = {
+  value: string;
+  label: string;
+};
+
+const TextSizeItems: TextSizeItem[] = [
+  {
+    value: "xs",
+    label: "xs",
+  },
+  {
+    value: "sm",
+    label: "sm",
+  },
+  {
+    value: "base",
+    label: "base",
+  },
+];
+
 const App = () => {
+  const [textSize, setTextSize] = useState("");
+
+  // 初回テキストサイズ反映
+  useEffect(() => {
+    const init = async () => {
+      const textSize = await loadTextSize();
+      setTextSize(textSize || "sm");
+    };
+    init();
+  }, []);
+
+  const handleChangeTextSize = useCallback(
+    (value: string) => {
+      setTextSize(value);
+    },
+    [setTextSize],
+  );
+
+  const handleSave = useCallback(async () => {
+    await saveTextSize(textSize);
+    toast("設定を保存しました", { duration: 1000 });
+  }, [textSize]);
+
   return (
     <div className="p-4">
       <div>
-        <h1>設定</h1>
+        <h1 className="text-2xl font-bold">設定ページ</h1>
 
-        <button
-          onClick={async () => {
-            await chrome.storage.local.set({
-              username: "yamada",
-            });
+        <div className="mt-6 grid grid-cols-1 gap-4">
+          <div className="grid grid-cols-[100px_1fr] gap-2">
+            <div>テキストサイズ</div>
+            <Select value={textSize} onValueChange={handleChangeTextSize}>
+              <SelectTrigger className="w-full max-w-48">
+                <SelectValue placeholder="テキストサイズを選択してください" />
+              </SelectTrigger>
+              <SelectContent>
+                {TextSizeItems.map((item) => (
+                  <SelectItem key={item.value} value={item.value}>
+                    {item.label}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
 
-            alert("saved");
-          }}
-        >
-          Save
-        </button>
+        <div className="mt-6">
+          <Button onClick={handleSave}>設定を保存</Button>
+        </div>
       </div>
     </div>
   );
--- src/pages/options/main.tsx
+++ src/pages/options/main.tsx
@@ -1,4 +1,5 @@
 import { StrictMode } from "react";
+import { Toaster } from "@/components/ui/sonner";
 
 import App from "./App";
 import "../../global.css";
@@ -7,5 +8,6 @@ import { createRoot } from "react-dom/client";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Toaster />
   </StrictMode>,
 );
