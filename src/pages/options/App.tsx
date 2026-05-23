d645af48b5015091776a82f130aa547fd2d07860
zakzakst <e1433104@gmail.com>
2026-05-23 14:25:40 +0900

    fix

--- src/pages/options/App.tsx
+++ src/pages/options/App.tsx
@@ -1,6 +1,18 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Field, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
 import {
   Select,
   SelectContent,
@@ -8,8 +20,13 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  type Template,
+  loadTemplates,
+  saveTemplates,
+} from "@/src/shared/storage/templates";
 import { loadTextSize, saveTextSize } from "@/src/shared/storage/textSize";
-import { saveTemplates } from "@/src/shared/storage/templates";
 import { toast } from "sonner";
 
 type TextSizeItem = {
@@ -34,15 +51,27 @@ const TextSizeItems: TextSizeItem[] = [
 
 const App = () => {
   const [textSize, setTextSize] = useState("");
+  const [templates, setTemplates] = useState<Template[]>([]);
+  const templateNameRef = useRef<HTMLInputElement>(null);
+  const templateTextRef = useRef<HTMLTextAreaElement>(null);
 
-  // 初回テキストサイズ反映
+  // テキストサイズ反映
   useEffect(() => {
     const init = async () => {
       const textSize = await loadTextSize();
       setTextSize(textSize || "sm");
     };
     init();
-  }, []);
+  }, [setTextSize]);
+
+  // テンプレート情報反映
+  useEffect(() => {
+    const init = async () => {
+      const templates = await loadTemplates();
+      setTemplates(templates);
+    };
+    init();
+  }, [setTemplates]);
 
   const handleChangeTextSize = useCallback(
     (value: string) => {
@@ -51,16 +80,37 @@ const App = () => {
     [setTextSize],
   );
 
+  const handleDeleteTemplate = useCallback(
+    (name: string) => {
+      const newTemplates = templates.filter(
+        (template) => template.name !== name,
+      );
+      setTemplates(newTemplates);
+    },
+    [templates, setTemplates],
+  );
+
+  const handleAddTemplate = useCallback(() => {
+    const nameEl = templateNameRef.current;
+    const textEl = templateTextRef.current;
+    if (!nameEl || !textEl) return;
+
+    if (nameEl.value && textEl.value) {
+      setTemplates((current) => [
+        ...current,
+        { name: nameEl.value, text: textEl.value },
+      ]);
+      nameEl.value = "";
+      textEl.value = "";
+      toast("テンプレートを追加しました", { duration: 1000 });
+    }
+  }, [setTemplates, templateNameRef, templateTextRef]);
+
   const handleSave = useCallback(async () => {
     await saveTextSize(textSize);
-    await saveTemplates([
-      {
-        name: "test",
-        text: "## test text",
-      },
-    ]);
+    await saveTemplates(templates);
     toast("設定を保存しました", { duration: 1000 });
-  }, [textSize]);
+  }, [templates, textSize]);
 
   return (
     <div className="p-4">
@@ -82,6 +132,69 @@ const App = () => {
                 ))}
               </SelectContent>
             </Select>
+          </div>
+        </div>
+
+        <div className="mt-6 grid grid-cols-1 gap-4">
+          <div className="grid grid-cols-[100px_1fr] gap-2">
+            <div>テンプレート</div>
+            <div>
+              {!templates.length && (
+                <div>登録されたテンプレートはありません</div>
+              )}
+              {!!templates.length && (
+                <ul className="list-disc">
+                  {templates.map((template) => (
+                    <li key={template.name} className="flex items-center gap-2">
+                      <span>{template.name}</span>
+                      <Button
+                        size="xs"
+                        onClick={() => handleDeleteTemplate(template.name)}
+                      >
+                        X
+                      </Button>
+                    </li>
+                  ))}
+                </ul>
+              )}
+              <div className="mt-2">
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button variant="outline">テンプレートを追加する</Button>
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>テンプレート追加フォーム</DialogTitle>
+                      <DialogDescription>
+                        フォームを入力してください
+                      </DialogDescription>
+                    </DialogHeader>
+                    <div>
+                      <Field>
+                        <FieldLabel htmlFor="template-name">
+                          テンプレート名
+                        </FieldLabel>
+                        <Input id="template-name" ref={templateNameRef} />
+                      </Field>
+                      <Field>
+                        <FieldLabel htmlFor="template-text">
+                          テンプレート文
+                        </FieldLabel>
+                        <Textarea id="template-text" ref={templateTextRef} />
+                      </Field>
+                    </div>
+                    <DialogFooter>
+                      <div className="grid grid-cols-2 gap-2">
+                        <DialogClose asChild>
+                          <Button variant="outline">閉じる</Button>
+                        </DialogClose>
+                        <Button onClick={handleAddTemplate}>追加</Button>
+                      </div>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
+              </div>
+            </div>
           </div>
         </div>
 
